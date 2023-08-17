6d617f66abde78c8c98f4e25b4324646631d388d
Philipp <50810270+PSum@users.noreply.github.com>
2023-08-17 13:41:18 +0200

    gs

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    length = word.length;
+    let output = "";
+    console.log(output)
+    for (let i = 0; i < word.length; i++) {
+        let index = length - i -1;
+        // console.log(index);
+        let letter = word[index];
+        output += letter;
+        // console.log(word[index]);
+    }
+    return output;
 };
 
+// reverseString("hello");
 // Do not edit below this line
 module.exports = reverseString;
