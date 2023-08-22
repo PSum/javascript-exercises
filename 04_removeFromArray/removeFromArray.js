e08256f12200c5040f91cd50ae6a1c5265ff9761
Philipp <50810270+PSum@users.noreply.github.com>
2023-08-22 22:34:20 +0200

    finished excercise 4 and 5

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,12 +1,25 @@
-function removeFromArray(arr, value) {
+function removeFromArray(arr, ...valuesToRemove) {
+  // Die Funktion erhält ein Array 'arr' und eine Liste von Werten 'valuesToRemove',
+  // die aus dem Array entfernt werden sollen.
+  // Die Syntax ...valuesToRemove wird in JavaScript als "Rest-Parameter" bezeichnet. Sie ermöglicht es, eine unbestimmte Anzahl von Argumenten als Array darzustellen.
+
+  // Iteriere über die Werte, die entfernt werden sollen.
+  for (const value of valuesToRemove) {
+    // Finde den Index des aktuellen Werts im Array.
     const index = arr.indexOf(value);
-  
+
+    // Überprüfe, ob der Wert im Array gefunden wurde.
     if (index !== -1) {
+      // Wenn der Wert gefunden wurde (Index ist nicht -1),
+      // entferne den Wert aus dem Array an der gefundenen Position.
       arr.splice(index, 1);
     }
-  
-    return arr;
+    // Wenn der Wert nicht gefunden wurde, wird dieser Schritt übersprungen.
   }
+
+  // Gib das bearbeitete Array zurück, unabhängig davon, ob Werte entfernt wurden oder nicht.
+  return arr;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,42 @@
-const sumAll = function() {
+// A function that generates an array of numbers between the given start and end values.
+function getNumbers(start, end) {
+    if (start > end) {
+        // Swap start and end if start is greater than end
+        [start, end] = [end, start];
+    }
 
+    const numbers = [];
+
+    // Loop to generate numbers and add them to the array.
+    for (let i = start; i <= end; i++) {
+        numbers.push(i);
+    }
+
+    return numbers; // Return the array of numbers.
+}
+
+// A function that calculates the sum of all numbers between two values.
+const sumAll = function(value1, value2) {
+    let sum = 0; // Initialize the sum to 0.
+
+    // Check for invalid inputs: negative values or non-number inputs.
+    if (value1 <= 0 || value2 <= 0 || typeof value1 !== "number" || typeof value2 !== "number") {
+        return "ERROR"; // Return an error message.
+    } else {
+        // Generate the array of numbers using the getNumbers function.
+        const numbers = getNumbers(value1, value2);
+
+        // Loop through the numbers and add them to the sum.
+        for (const number of numbers) {
+            sum += parseInt(number);
+        }
+
+        return sum; // Return the sum of the numbers.
+    }
 };
 
+// Call the sumAll function with the values 3 and 7.
+sumAll(3, 7);
+
 // Do not edit below this line
-module.exports = sumAll;
+module.exports = sumAll; // Export the sumAll function for use in other modules.
