09619e3e4e0b76b5027ac85689cd506024255a35
Philipp <50810270+PSum@users.noreply.github.com>
2023-08-22 19:28:13 +0200

    Update removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
-
-};
+function removeFromArray(arr, value) {
+    const index = arr.indexOf(value);
+  
+    if (index !== -1) {
+      arr.splice(index, 1);
+    }
+  
+    return arr;
+  }
 
 // Do not edit below this line
 module.exports = removeFromArray;
