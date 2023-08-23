dbca5641f767c833096f25385edf60d462d303d8
Philipp <50810270+PSum@users.noreply.github.com>
2023-08-23 14:21:52 +0200

    solved excercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(number) {
+  let numberCel = (number - 32) * (5/9);
+  let rounded = Math.round(numberCel * 10) / 10
+  return rounded
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(number) {
+  let numberFar = (number * (9/5)) +32;
+  let rounded = Math.round(numberFar * 10) / 10
+  return rounded
 };
 
 // Do not edit below this line
@@ -9,3 +15,5 @@ module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
+// var rounded = Math.round(number * 10) / 10
