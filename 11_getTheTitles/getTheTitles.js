b7edbbf3d05e18ca5c6c77ef796c60fe5cf0a0dd
psum <sum.philipp@gmail.com>
2023-09-22 18:49:54 +0200

    finished exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(values) {
+    let list = [];
+    let value;
+    values.forEach(element => {
+        value = element["title"];
+        list.push(value);
+    });
+    return list;
 };
 
 // Do not edit below this line
