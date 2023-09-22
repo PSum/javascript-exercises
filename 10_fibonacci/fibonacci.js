aa0e397d583a3776373a2fb42b419fcafe510cad
psum <sum.philipp@gmail.com>
2023-09-22 18:31:53 +0200

    solved exc 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,10 +1,17 @@
 const fibonacci = function(value) {
     ammount = parseInt(value);
+    if (ammount == 1){
+        return 1;
+    }else if (ammount < 0) {
+        return "OOPS";
+    }
+    else
+    {
     let one = 0;
     let two = 1; // 0 1 2
     let result = 0;
 
-    for (let i = 0; i < ammount; i++){
+    for (let i = 1; i < ammount; i++){
         result = one + two; // 
         one = two;
         two = result;
@@ -12,7 +19,7 @@ const fibonacci = function(value) {
         // console.log(`Result = ${result} and one = ${one} and two = ${two}`);
     }
     return result;
-
+    }
 };
 
 // Do not edit below this line
