e6b5fea4219356427957e574a3ba4799650e83c4
Philipp <50810270+PSum@users.noreply.github.com>
2023-09-22 18:24:27 +0200

    update

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(value) {
+    ammount = parseInt(value);
+    let one = 0;
+    let two = 1; // 0 1 2
+    let result = 0;
+
+    for (let i = 0; i < ammount; i++){
+        result = one + two; // 
+        one = two;
+        two = result;
+
+        // console.log(`Result = ${result} and one = ${one} and two = ${two}`);
+    }
+    return result;
 
 };
 
